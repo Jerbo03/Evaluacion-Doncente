a0e01ea3dfdb7bbb7dff68858fdc7a9583824cb6
José André Paredes Quispe <90292121+Jerbo03@users.noreply.github.com>
2024-11-05 18:22:49 -0500

    Automáticamente reconocer el número de áreas y preguntas

--- UCSM.js
+++ UCSM.js
@@ -1,20 +1,29 @@
-// Max es el valor máximo para evaluar el docente (1-5)
-// Min es el valor mínimo para evaluar el docente (1-5)
-// nPr es el array de preguntas por sección en la encuesta
-function evaluarUCSM(max, min, nPr = [3,5,2,1,1,1,1,1]) {
-  	if (max>5 || min>5 || max<1 || max<1) throw("Max o min fuera de rango");
-	
-	min--;
-	for (let i=0; i<nPr.length; i++) {
-		for (let j=0; j<nPr[i]; j++) {
-			let r = Math.floor(Math.random() * (max - min) + min);
-			let id = `MainContent_GridViewPreguntasGral_GridViewPreguntas_${i}_RadioButtonListOpciones_${j}_${r}_${j}`;
-			let rad = document.getElementById(id);
-	    rad.checked = true;
-	}
-	}
+function evaluarUCSM(max, min) {
+    if (max > 5 || min > 5 || max < 1 || min < 1) throw ("Max o min fuera de rango");
+    
+    min--;
+    let i = 0;
+    let j = 0;
 
-	document.getElementById('MainContent_btnGuardar').click(); // Pasar automáticamente al siguiente
+    while (true) {
+        let r = Math.floor(Math.random() * (max - min) + min);
+        let id = `MainContent_GridViewPreguntasGral_GridViewPreguntas_${i}_RadioButtonListOpciones_${j}_${r}_${j}`;
+        let rad = document.getElementById(id);
+
+        if (rad) {
+            rad.checked = true;
+            j++;
+        } else {
+            if (j > 0) {
+                i++;
+                j = 0;
+            } else {
+                break;
+            }
+        }
+    }
+
+    document.getElementById('MainContent_btnGuardar').click(); // Pasar automáticamente al siguiente
 }
 
-// E.g. evaluarUCSM(5,4);
+evaluarUCSM(5, 4);
